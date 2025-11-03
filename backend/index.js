f06d72b99f50f4f4b347b2813ebaccf1c0f6e5c8
My Name <sainaveen@Sais-MacBook-Air.local>
2025-11-03 20:27:35 +0000

    initial changes

---
+++ backend/index.js
@@ -0,0 +1,48 @@
+require("dotenv").config();
+
+//packages
+const cors = require("cors");
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+// CORS config
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (process.env.ALLOWED_HOSTS.indexOf(origin) !== -1 || !origin)
+        callback(null, true);
+      else callback(new Error("Not allowed by CORS"));
+    },
+  })
+);
+
+//database connection
+(async function () {
+  try {
+    const port = process.env.PORT;
+    app.listen(port, () => console.log(`Running on port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+})();
+
+app.use((req, res, next) => {
+  res.setHeader("Expect-CT", "max-age=86400, enforce");
+  next();
+});
+
+// Routes
+app.use("/", (req, res, next) => {
+  res.send("chatbot there! Welcome.");
+});
+
+// if no route found
+// app.all("*", (req, res, next) => {
+//   const url = req.originalUrl;
+//   next({ status: 404, message: `Can't find ${url} on this server!` });
+// });
+
+module.exports = app;
---
+++ backend/prisma.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig, env } from "prisma/config";
+require("dotenv").config();
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  engine: "classic",
+  datasource: {
+    url: env("DATABASE_URL"),
+  },
+});
